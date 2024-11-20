f114c43482970c84ed6abe18a9b4eae41dee1b74
msrkrk <misra.kirik@gmail.com>
2024-11-20 20:07:17 +0300

    relations added.

--- BiteSizedDietPlan_DAL/Context/AppDbContext.cs
+++ BiteSizedDietPlan_DAL/Context/AppDbContext.cs
@@ -11,6 +11,8 @@ namespace BiteSizedDietPlan_DAL.Context
     public class AppDbContext : DbContext
     {
         public DbSet<User> Users { get; set; }
+        public DbSet<MealCategory> MealCategories { get; set; }
+        public DbSet<Meal> Meals { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("server=10.211.55.2;database=BiteSizedDietPlanDb;user id=sa;password=Qwq1234.;trustservercertificate=true;");
@@ -18,10 +20,25 @@ namespace BiteSizedDietPlan_DAL.Context
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<User>()
-                .HasIndex(u => u.Email)
-                .IsUnique(); // E-posta alanını benzersiz yapar
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>().HasIndex(u => u.Email).IsUnique(); // E-posta alanını benzersiz yapar.
+
+            // MealsCategories ile Meal arasında bire-çok ilişkiyi tanımlandı.
+            modelBuilder.Entity<Meal>()
+                .HasOne(m => m.MealCategory)
+                .WithMany(c => c.Meals)
+                .HasForeignKey(m => m.MealCategoryId);
+
+            //Seed data ile yemek kategorileri eklendi.
+            modelBuilder.Entity<MealCategory>().HasData(
+            new MealCategory { Id = 1, Name = "Kahvaltılıklar" },
+            new MealCategory { Id = 2, Name = "Öğle Yemekleri" },
+            new MealCategory { Id = 3, Name = "Akşam Yemekleri" },
+            new MealCategory { Id = 4, Name = "Atıştırmalıklar" });
+
         }
+
 
     }
 }
---
+++ BiteSizedDietPlan_DAL/Entities/Meal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiteSizedDietPlan_DAL.Entities
+{
+    public class Meal : BaseClass
+    {
+        public string Name { get; set; }
+
+        public int Calorie { get; set; }
+
+        public int MealCategoryId { get; set; }
+        public MealCategory MealCategory { get; set; }
+
+    }
+}
